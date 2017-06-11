ca09bbbd32b9112bc41e4f822dd00b83ff501375
Francisco Maseda Muiño <frmadem@gmail.com>
2017-06-11 16:04:28 +0200

    Agregado soporte para productos

--- index.js
+++ index.js
@@ -2,36 +2,93 @@ const CE = require("catro-eixos-js");
 
 const UUIDV4 = require("uuid/v4");
 
+const TratarError = require("./lib/error.js");
+const CrearProductos = require("./lib/crear_productos.js");
+
 class Tramitador{
 
-    constructor(procesador){
+    constructor(procesador, swaggerRef){
         
         this.procesador = procesador;
+        this.swaggerRef = swaggerRef;
+
+        this.crearProductos();
     }
 
-    tramitar(proceso, req, res){
+    crearProductos(){
 
-        let tarea = this.__crearTarea(proceso, req.swagger.params);
+        this.productos = new CrearProductos(
+
+            this.swaggerRef
+
+        ).crear();
+
+        console.log(this.productos);
+
+    }
+
+    tramitar(proceso, p = {}){
+
+        proceso = this.__transformarProceso(proceso);
+
+        let tarea = this.__crearTarea(proceso, p.args, p.producto);
 
         this.procesador.ejecutar(tarea)
 
             .then((tarea) => {
+                
+                p.res.setHeader('Content-Type', 'application/json');
 
-                res.json(tarea.resultados);
+                let resultados = tarea.resultados;
+
+                if(tarea.resultados.__tiene_producto__){
+                    resultados = resultados.__producto__.getValores();
+                }
+
+                p.res.end(JSON.stringify(resultados));
 
             })
 
             .catch((err) => {
 
-                res.json(tarea.resultados);
+                p.res.setHeader('Content-Type', 'application/json');
+                //new TratarError(tarea, res).tratar();
+
+                p.res.end();
 
             })
 
     }
 
-    __crearTarea(proceso, args){
+    __transformarProceso(p){
 
-        args.proceso
+        p = p.replace(/([A-Z])/g, function(a, b){
+        
+            return "_" + a.toLowerCase();
+        
+        });
+        
+        let partes = p.match(/(\_[a-z]+)$/);
+        
+        let verbo = p.replace(/(\_[a-z]+$)/, "");
+        
+        let entidad = partes[1].replace(/_/g, "").replace(/^(\w)/, function(a) { return a.toUpperCase()});
+        
+        if(entidad.match(/s$/)){
+            entidad = entidad.replace(/s$/, "");
+            verbo += "_todos";
+        }
+ 
+        return entidad + "." + verbo;      
+
+    }
+
+    __crearTarea(proceso, args, producto){
+
+        args = this.__prepararArgsParaTarea(args);
+        
+        args.proceso = proceso;
+        args.__producto__ = this.productos[producto];
 
         return new CE.Tarea(
 
@@ -42,7 +99,27 @@ class Tramitador{
         );
         
     }
+
+    __prepararArgsParaTarea(args){
+
+        let t_args = {};
+
+        Object.keys(args).forEach((k) => {
+
+            t_args[k] = args[k].value;
+        });
+
+        t_args["swagger_args"] = Object.assign({}, args);
+
+        return t_args;
+    }
+
 }
 
+module.exports = {
 
-module.exports = Tramitador;
+    "Tramitador": Tramitador,
+
+    "ProcesoSwagger": require("./lib/proceso_swagger.js")
+
+}
